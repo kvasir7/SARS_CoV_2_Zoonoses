2d7ba04f93cb4fbdf38c190963eca44f2630bdd2
Joe Madison <Joseph.Madison@coyotes.usd.edu>
2020-05-11 05:52:15 -0500

    Update SEIR.py

--- SEIR.py
+++ SEIR.py
@@ -5,32 +5,34 @@ import matplotlib.pyplot as plt
 # Total population, N.
 N = 100
 # Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = 1, 0
+E0, I0, R0 = 20, 70, 10
 # Everyone else, S0, is susceptible to infection initially.
-S0 = N - I0 - R0
-# Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 0./14 
+S0 = N - E0 - I0 - R0
+# Contact rate, beta, incubation rate, sigma, and mean recovery rate, gamma, (in 1/days).
+beta, sigma, gamma = 0.2, 0.1, 0./14 
 # A grid of time points (in days)
 t = np.linspace(0, 160, 160)
 
 # The SIR model differential equations.
-def deriv(y, t, N, beta, gamma):
-    S, I, R = y
+def deriv(y, t, N, beta, sigma, gamma):
+    S, E, I, R = y
     dSdt = -beta * S * I / N
-    dIdt = beta * S * I / N - gamma * I
+    dEdt = beta * S * I / N - sigma * E
+    dIdt = sigma * E - gamma * I
     dRdt = gamma * I
     return dSdt, dIdt, dRdt
 
 # Initial conditions vector
-y0 = S0, I0, R0
-# Integrate the SIR equations over the time grid, t.
-ret = odeint(deriv, y0, t, args=(N, beta, gamma))
-S, I, R = ret.T
+y0 = S0, E0, I0, R0
+# Integrate the SEIR equations over the time grid, t.
+ret = odeint(deriv, y0, t, args=(N, beta, sigma, gamma))
+S, E, I, R = ret.T
 
-# Plot the data on three separate curves for S(t), I(t) and R(t)
+# Plot the data on four separate curves for S(t), E(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111, axisbelow=True)
 ax.plot(t, S/100, 'b', alpha=0.5, lw=2, label='Susceptible')
+ax.plot(t, E/100, 'y', alpha=0.5, lw=2, label='Exposed')
 ax.plot(t, I/100, 'r', alpha=0.5, lw=2, label='Infected')
 ax.plot(t, R/100, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
 ax.set_xlabel('Time /days')
